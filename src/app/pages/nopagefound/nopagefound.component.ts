547085e1096c7540649bfd3f1a606fc18fda2378
JL-OLEMAR <joseluis19963@gmail.com>
2021-12-06 11:39:24 -0500

    add librerias externas

---
+++ src/app/pages/nopagefound/nopagefound.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-nopagefound',
+  templateUrl: './nopagefound.component.html',
+  styles: []
+})
+export class NopagefoundComponent implements OnInit {
+  // constructor() { }
+
+  ngOnInit (): void {
+  }
+}
