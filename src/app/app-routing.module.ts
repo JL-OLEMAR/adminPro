a30e1197238e93827f4948a6dfa53a5c38aa420a
JL-OLEMAR <joseluis19963@gmail.com>
2021-12-08 10:34:25 -0500

    Add module auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
-import { DashboardComponent } from './pages/dashboard/dashboard.component'
 import { LoginComponent } from './auth/login/login.component'
 import { RegisterComponent } from './auth/register/register.component'
+import { PagesComponent } from './pages/pages.component'
+import { DashboardComponent } from './pages/dashboard/dashboard.component'
 import { ProgressComponent } from './pages/progress/progress.component'
 import { Grafica1Component } from './pages/grafica1/grafica1.component'
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component'
-import { PagesComponent } from './pages/pages.component'
+import { NopagefoundComponent } from './nopagefound/nopagefound.component'
 
 const routes: Routes = [
   {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,22 @@ import { BrowserModule } from '@angular/platform-browser'
 // Modules
 import { AppRoutingModule } from './app-routing.module'
 import { PagesModule } from './pages/pages.module'
+import { AuthModule } from './auth/auth.module'
 
 // Components
 import { AppComponent } from './app.component'
-import { LoginComponent } from './auth/login/login.component'
-import { RegisterComponent } from './auth/register/register.component'
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component'
+import { NopagefoundComponent } from './nopagefound/nopagefound.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
     NopagefoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PagesModule
+    PagesModule,
+    AuthModule
   ],
   bootstrap: [AppComponent]
 })
