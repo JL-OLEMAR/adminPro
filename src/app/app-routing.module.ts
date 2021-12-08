d593c12d2f334e1cc18e4ca1a46f10d487322157
JL-OLEMAR <joseluis19963@gmail.com>
2021-12-08 10:52:02 -0500

    Add rutas hijas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,35 +1,25 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
+// Modules
+import { PagesRoutingModule } from './pages/pages.routing'
+
+// Components
 import { LoginComponent } from './auth/login/login.component'
 import { RegisterComponent } from './auth/register/register.component'
-import { PagesComponent } from './pages/pages.component'
-import { DashboardComponent } from './pages/dashboard/dashboard.component'
-import { ProgressComponent } from './pages/progress/progress.component'
-import { Grafica1Component } from './pages/grafica1/grafica1.component'
 import { NopagefoundComponent } from './nopagefound/nopagefound.component'
 
 const routes: Routes = [
-  {
-    // Rutas hijas ↓
-    path: '',
-    component: PagesComponent,
-    children: [
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'progress', component: ProgressComponent },
-      { path: 'grafica1', component: Grafica1Component },
-      { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
-    ]
-  },
-
-  // Rutas de autenticación ↓
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   { path: '**', component: NopagefoundComponent }
 ]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    PagesRoutingModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
