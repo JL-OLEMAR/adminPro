6bdb4b7cbbf8456593e95e2a7cec28ae1e7c94a0
JL-OLEMAR <joseluis19963@gmail.com>
2021-12-08 01:56:53 -0500

    Add rutas hijas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,14 +7,24 @@ import { RegisterComponent } from './auth/register/register.component'
 import { ProgressComponent } from './pages/progress/progress.component'
 import { Grafica1Component } from './pages/grafica1/grafica1.component'
 import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component'
+import { PagesComponent } from './pages/pages.component'
 
 const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'login', component: LoginComponent },
+  {
+    // Rutas hijas ↓
+    path: '',
+    component: PagesComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'progress', component: ProgressComponent },
+      { path: 'grafica1', component: Grafica1Component },
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
+    ]
+  },
+
+  // Rutas de autenticación ↓
   { path: 'register', component: RegisterComponent },
-  { path: 'progress', component: ProgressComponent },
-  { path: 'grefica1', component: Grafica1Component },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
   { path: '**', component: NopagefoundComponent }
 ]
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component'
 import { HeaderComponent } from './shared/header/header.component'
 import { ProgressComponent } from './pages/progress/progress.component'
 import { Grafica1Component } from './pages/grafica1/grafica1.component'
+import { PagesComponent } from './pages/pages.component'
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { Grafica1Component } from './pages/grafica1/grafica1.component'
     SidebarComponent,
     HeaderComponent,
     ProgressComponent,
-    Grafica1Component
+    Grafica1Component,
+    PagesComponent
   ],
   imports: [
     BrowserModule,
