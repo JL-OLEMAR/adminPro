b5e757492abe2d9a2413d1a2112516b05cc06435
JL-OLEMAR <joseluis19963@gmail.com>
2021-12-09 14:32:07 -0500

    Add atibutos personalizados en ProgressComponent

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { FormsModule } from '@angular/forms'
 import { RouterModule } from '@angular/router'
 
 import { SharedModule } from '../shared/shared.module'
@@ -24,6 +25,7 @@ import { PagesComponent } from './pages.component'
   ],
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule,
     SharedModule
   ]
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -1,13 +1,29 @@
-import { Component, OnInit } from '@angular/core'
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
   styleUrls: ['./progress.component.css']
 })
-export class ProgressComponent implements OnInit {
-  // constructor () { }
+export class ProgressComponent {
+  progreso: number = 50
 
-  ngOnInit (): void {
+  get getPorcentaje (): string {
+    return `${this.progreso}%`
+  }
+
+  cambiarValor (valor: number): number {
+    if (this.progreso >= 100 && valor >= 0) {
+      this.progreso = 100
+      return this.progreso
+    }
+
+    if (this.progreso <= 0 && valor < 0) {
+      this.progreso = 0
+      return this.progreso
+    }
+
+    this.progreso = this.progreso + valor
+    return this.progreso
   }
 }
