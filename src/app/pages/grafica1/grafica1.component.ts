d80b202dfb6228246b32cb62118e3bf164f2e0d5
JL-OLEMAR <joseluis19963@gmail.com>
2021-12-12 02:52:43 -0500

    Add canvas para mostrar grafica

--- src/app/pages/grafica1/grafica1.component.ts
+++ src/app/pages/grafica1/grafica1.component.ts
@@ -1,13 +1,19 @@
-import { Component, OnInit } from '@angular/core'
+import { Component } from '@angular/core'
+import { Color, Label, MultiDataSet } from 'ng2-charts'
 
 @Component({
   selector: 'app-grafica1',
   templateUrl: './grafica1.component.html',
   styles: []
 })
-export class Grafica1Component implements OnInit {
-  // constructor () { }
+export class Grafica1Component {
+  // Doughnut
+  public doughnutChartLabels: Label[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales']
+  public doughnutChartData: MultiDataSet = [
+    [350, 450, 100]
+  ]
 
-  ngOnInit (): void {
-  }
+  public colors: Color[] = [
+    { backgroundColor: ['#6857E6', '#009FEE', '#F02059'] }
+  ]
 }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { FormsModule } from '@angular/forms'
 import { RouterModule } from '@angular/router'
+import { ChartsModule } from 'ng2-charts'
 
 // Modules
 import { SharedModule } from '../shared/shared.module'
@@ -30,6 +31,7 @@ import { PagesComponent } from './pages.component'
     CommonModule,
     FormsModule,
     RouterModule,
+    ChartsModule,
     SharedModule,
     ComponentsModule
   ]
