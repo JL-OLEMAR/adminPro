c39a02805cdc65fc7cf6029cd76b3d7f570952ba
JL-OLEMAR <joseluis19963@gmail.com>
2021-12-15 15:26:42 -0500

    Service para controlar el sidebar

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+import { SidebarService } from '../../services/sidebar.service'
 
 @Component({
   selector: 'app-sidebar',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core'
   styles: []
 })
 export class SidebarComponent implements OnInit {
-  // constructor () { }
+  menuItems: any[] = []
+
+  constructor (private readonly sidebarService: SidebarService) {
+    this.menuItems = this.sidebarService.menu
+    console.log(this.menuItems)
+  }
 
   ngOnInit (): void {
   }
