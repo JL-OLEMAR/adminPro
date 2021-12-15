cb6b934b4cb05a21bbac433f94b875ccf4bf3a56
JL-OLEMAR <joseluis19963@gmail.com>
2021-12-15 14:22:12 -0500

    RouterLink

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { RouterModule } from '@angular/router'
 
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component'
 import { SidebarComponent } from './sidebar/sidebar.component'
@@ -16,6 +17,9 @@ import { HeaderComponent } from './header/header.component'
     SidebarComponent,
     HeaderComponent
   ],
-  imports: [CommonModule]
+  imports: [
+    CommonModule,
+    RouterModule
+  ]
 })
 export class SharedModule { }
