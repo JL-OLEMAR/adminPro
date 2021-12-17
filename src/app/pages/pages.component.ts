f5176f8296b562dbf9cefb6dc1af5c0656ca39ef
JL-OLEMAR <joseluis19963@gmail.com>
2021-12-17 11:31:55 -0500

    Add funcion customInitFuctions de forma global

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core'
-import { SettingsService } from '../services/settings.service'
 
+/*
+  La funcion customInitFuctions(), sirve para ejecutar funciones globales.
+  la cual se encuentra en el path ./assets/js/custom.min.js
+*/
+declare function customInitFuctions (): void
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
   styles: []
 })
 export class PagesComponent implements OnInit {
-  constructor (private readonly settingsService: SettingsService) {}
-
   ngOnInit (): void {
+    customInitFuctions()
   }
 }
