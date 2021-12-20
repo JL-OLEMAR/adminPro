eaee2a2741cb78930c7466eb88aa3ed692b758a0
JL-OLEMAR <joseluis19963@gmail.com>
2021-12-19 22:54:05 -0500

    Observable operador filter

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core'
 import { Observable, interval } from 'rxjs'
-import { retry, take, map } from 'rxjs/operators' // eslint-disable-line
+import { filter, map, retry, take } from 'rxjs/operators' // eslint-disable-line @typescript-eslint/no-unused-vars
 
 @Component({
   selector: 'app-rxjs',
@@ -23,10 +23,11 @@ export class RxjsComponent {
 
   retornaIntervalo (): Observable<number> {
     // el interval se maneja en milisegundos 1000ms = 1s
-    return interval(1000)
+    return interval(500)
       .pipe(
-        take(4), // el take es para que se detenga el observable despues de 4 iteraciones
-        map(valor => (valor + 1))
+        take(10), // el take es para que se detenga el observable despues de 10 iteraciones
+        map(valor => (valor + 1)),
+        filter(valor => ((valor % 2 === 0) && true))
       )
   }
 
