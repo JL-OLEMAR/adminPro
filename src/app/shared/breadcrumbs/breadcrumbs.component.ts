a64eb6fa3284caff1d31936a4e6174eff2be6f3a
JL-OLEMAR <joseluis19963@gmail.com>
2021-12-20 23:09:38 -0500

    Optimizacion del breadcrumbs

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core'
-import { ActivationEnd, Router } from '@angular/router'
+import { Component, OnDestroy } from '@angular/core'
+import { ActivationEnd, Data, Router } from '@angular/router'
+import { Subscription, Observable } from 'rxjs'
 import { filter, map } from 'rxjs/operators'
 
 @Component({
@@ -7,23 +8,28 @@ import { filter, map } from 'rxjs/operators'
   templateUrl: './breadcrumbs.component.html',
   styles: []
 })
-export class BreadcrumbsComponent {
+export class BreadcrumbsComponent implements OnDestroy {
   public titulo!: string
+  public tituloSubs$!: Subscription
 
   constructor (private readonly router: Router) {
-    this.getArgumentosRuta()
+    this.tituloSubs$ = this.getArgumentosRuta()
+      .subscribe(({ titulo }) => {
+        this.titulo = titulo
+        document.title = `AdminPro - ${(titulo) as string}`
+      })
   }
 
-  getArgumentosRuta (): void {
-    this.router.events
+  ngOnDestroy (): void {
+    this.tituloSubs$.unsubscribe()
+  }
+
+  getArgumentosRuta (): Observable<Data> {
+    return this.router.events
       .pipe(
         filter((event): event is ActivationEnd => event instanceof ActivationEnd),
         filter((event: ActivationEnd) => event.snapshot.firstChild === null),
         map((event: ActivationEnd) => event.snapshot.data)
       )
-      .subscribe(({ titulo }) => {
-        this.titulo = titulo
-        document.title = `AdminPro - ${(titulo) as string}`
-      })
   }
 }
