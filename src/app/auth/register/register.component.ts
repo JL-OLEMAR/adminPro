7f2cc321d7c15e6926f84662f51f1cd21a49f38f
JL-OLEMAR <joseluis19963@gmail.com>
2022-01-05 02:46:39 -0500

    Validaciones del formulario register

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -7,17 +7,38 @@ import { FormBuilder, Validators } from '@angular/forms'
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  public formSubmitted: boolean = false
+
   public registerForm = this.fb.group({
-    nombre: ['Pepe', [Validators.required, Validators.minLength(3)]],
-    email: ['test100@test.com', [Validators.required, Validators.email]],
-    password: ['123456', Validators.required],
-    password2: ['123456', Validators.required],
+    nombre: ['', [Validators.required, Validators.minLength(3)]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+    password2: ['', Validators.required],
     terminos: [false, Validators.required]
   })
 
   constructor (private readonly fb: FormBuilder) { }
 
   crearUsuario (): void {
+    this.formSubmitted = true
     console.log(this.registerForm.value)
+
+    if (this.registerForm.valid) {
+      console.log('Posteando formulario')
+    } else {
+      console.log('Formulario no es correcto')
+    }
+  }
+
+  campoNoValido (campo: string): boolean {
+    if (this.registerForm.controls[campo].invalid && this.formSubmitted) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  aceptaTerminos (): boolean {
+    return (!(this.registerForm.controls['terminos'].value as boolean) && this.formSubmitted)
   }
 }
