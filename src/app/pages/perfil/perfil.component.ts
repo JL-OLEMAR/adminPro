de6c37563437a90ba9c4dc64f27303ed82f2cb3a
JL-OLEMAR <joseluis19963@gmail.com>
2022-01-10 21:44:38 -0500

    Fixed actualizarUsuario

--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 import { FormGroup, FormBuilder, Validators } from '@angular/forms'
+
+import { Usuario } from '../../models/usuario.model'
 import { UsuarioService } from '../../services/usuario.service'
 
 @Component({
@@ -9,23 +11,26 @@ import { UsuarioService } from '../../services/usuario.service'
 })
 export class PerfilComponent implements OnInit {
   public perfilForm!: FormGroup
+  public usuario!: Usuario
 
   constructor (
     private readonly fb: FormBuilder,
     private readonly usuarioService: UsuarioService
-  ) { }
+  ) { this.usuario = usuarioService.usuario }
 
   ngOnInit (): void {
     this.perfilForm = this.fb.group({
-      nombre: ['Luis Olemar', [Validators.required, Validators.minLength(3)]],
-      email: ['test1@test.com', [Validators.required, Validators.email]]
+      nombre: [this.usuario.nombre, [Validators.required, Validators.minLength(3)]],
+      email: [this.usuario.email, [Validators.required, Validators.email]]
     })
   }
 
   actualizarPerfil (): void {
     this.usuarioService.actualizarUsuario(this.perfilForm.value)
-      .subscribe(resp => {
-        console.log(resp)
+      .subscribe(() => {
+        const { nombre, email } = this.perfilForm.value
+        this.usuario.nombre = nombre
+        this.usuario.email = email
       })
   }
 }
