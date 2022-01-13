15c682486894f6f5ccd34911e3fa48dc1e41aaec
JL-OLEMAR <joseluis19963@gmail.com>
2022-01-13 02:46:53 -0500

    Feat paginacion de getAllUsers

--- src/app/pages/mantenimientos/usuarios/usuarios.component.ts
+++ src/app/pages/mantenimientos/usuarios/usuarios.component.ts
@@ -10,14 +10,31 @@ import { UsuarioService } from '../../../services/usuario.service'
 export class UsuariosComponent implements OnInit {
   public totalUsers: number = 0
   public users: Usuario[] = []
+  public desde: number = 0
 
   constructor (private readonly usuarioService: UsuarioService) { }
 
   ngOnInit (): void {
-    this.usuarioService.getAllUsers()
+    this.cargarUsuarios()
+  }
+
+  cargarUsuarios (): void {
+    this.usuarioService.getAllUsers(this.desde)
       .subscribe(({ usuarios, total }) => {
         this.users = usuarios
         this.totalUsers = total
       })
+  }
+
+  cambiarPagina (valor: number): any {
+    this.desde += valor
+
+    if (this.desde < 0) {
+      this.desde = 0
+    } else if (this.desde >= this.totalUsers) {
+      this.desde -= valor
+    }
+
+    this.cargarUsuarios()
   }
 }
