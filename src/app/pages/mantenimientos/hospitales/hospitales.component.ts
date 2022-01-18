485a4d345103055b88fc39d17a83fbfaace296bd
JL-OLEMAR <joseluis19963@gmail.com>
2022-01-18 08:49:17 -0500

    Hospital CRUD functionality

--- src/app/pages/mantenimientos/hospitales/hospitales.component.ts
+++ src/app/pages/mantenimientos/hospitales/hospitales.component.ts
@@ -1,20 +1,38 @@
-import { Component, OnInit } from '@angular/core'
+/* eslint-disable @typescript-eslint/no-floating-promises */
+import { Component, OnDestroy, OnInit } from '@angular/core'
+import { Subscription } from 'rxjs'
+import { delay } from 'rxjs/operators'
+import Swal from 'sweetalert2'
+
 import { Hospital } from '../../../models/hospital.model'
 import { HospitalService } from '../../../services/hospital.service'
+import { ModalImagenService } from '../../../services/modal-imagen.service'
 
 @Component({
   selector: 'app-hospitales',
   templateUrl: './hospitales.component.html',
   styles: []
 })
-export class HospitalesComponent implements OnInit {
+export class HospitalesComponent implements OnInit, OnDestroy {
   public hospitales: Hospital[] = []
   public loading: boolean = false
+  public imgSubs!: Subscription
 
-  constructor (private readonly _hospitalService: HospitalService) { }
+  constructor (
+    private readonly _hospitalService: HospitalService,
+    private readonly _modalImagenService: ModalImagenService
+  ) { }
 
   ngOnInit (): void {
     this.getHospitals()
+
+    this.imgSubs = this._modalImagenService.newImg
+      .pipe(delay(100))
+      .subscribe(() => this.getHospitals())
+  }
+
+  ngOnDestroy (): void {
+    this.imgSubs.unsubscribe()
   }
 
   getHospitals (): void {
@@ -25,5 +43,39 @@ export class HospitalesComponent implements OnInit {
         this.loading = false
       }
       )
+  }
+
+  editarHospital (hosp: Hospital): void {
+    this._hospitalService.updateHospital(hosp._id ?? '', hosp.nombre)
+      .subscribe(() => { Swal.fire('Hospital actualizado', hosp.nombre, 'success') })
+  }
+
+  async deleteHospital (hosp: Hospital): Promise<void> {
+    this._hospitalService.deleteHospital(hosp._id ?? '')
+      .subscribe(() => {
+        this.getHospitals()
+        Swal.fire('Hospital deleted', hosp.nombre, 'success')
+      })
+  }
+
+  async openSweetAlertForm (): Promise<any> {
+    const { value } = await Swal.fire<string>({
+      title: 'New hospital',
+      text: 'Enter the name of the new hospital',
+      input: 'text',
+      inputPlaceholder: 'Hospital name',
+      showCancelButton: true
+    })
+
+    if ((value ?? '').trim().length > 0) {
+      this._hospitalService.newHospital(value ?? '')
+        .subscribe((resp: any) => {
+          this.hospitales.push(resp.hospital)
+        })
+    }
+  }
+
+  openModal (hosp: Hospital): void {
+    this._modalImagenService.openModal('hospitales', hosp._id ?? '', hosp.img)
   }
 }
