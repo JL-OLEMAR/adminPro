e3d4beb7f70c467922e7ba9fae0b6e7fc7af415f
JL-OLEMAR <joseluis19963@gmail.com>
2022-01-20 12:19:47 -0500

    feat(medico): show hospital info on medical page

--- src/app/pages/mantenimientos/medicos/medico.component.ts
+++ src/app/pages/mantenimientos/medicos/medico.component.ts
@@ -1,13 +1,53 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnDestroy, OnInit } from '@angular/core'
+import { FormGroup, FormBuilder, Validators } from '@angular/forms'
+import { Subscription } from 'rxjs'
+
+import { Hospital } from '../../../interfaces/getAllHospitals.interface'
+import { HospitalService } from '../../../services/hospital.service'
 
 @Component({
   selector: 'app-medico',
   templateUrl: './medico.component.html',
   styles: []
 })
-export class MedicoComponent implements OnInit {
-  // constructor () { }
+export class MedicoComponent implements OnInit, OnDestroy {
+  public medicoForm!: FormGroup
+  public hospitals: Hospital[] = []
+  public hospitalSelect: Hospital | undefined
+  private _hospitalSubs!: Subscription
+  private _hospitalSelectSubs!: Subscription
+
+  constructor (
+    private readonly _fb: FormBuilder,
+    private readonly _hospitalService: HospitalService
+  ) { }
 
   ngOnInit (): void {
+    this.medicoForm = this._fb.group({
+      nombre: ['Pepe', [Validators.required, Validators.minLength(3)]],
+      hospital: ['', Validators.required]
+    })
+    this.getHospitals()
+
+    this._hospitalSelectSubs = this.medicoForm.controls['hospital'].valueChanges
+      .subscribe(hospitalId => {
+        this.hospitalSelect = this.hospitals.find(h => h._id === hospitalId)
+      })
+  }
+
+  ngOnDestroy (): void {
+    this._hospitalSubs.unsubscribe()
+    this._hospitalSelectSubs.unsubscribe()
+  }
+
+  getHospitals (): void {
+    this._hospitalSubs = this._hospitalService.getAllHospitals()
+      .subscribe((hospitales: Hospital[]) => {
+        this.hospitals = hospitales
+      })
+  }
+
+  guardarMedico (): void {
+    console.log(this.medicoForm.value)
   }
 }
