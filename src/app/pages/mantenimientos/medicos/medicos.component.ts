a9586ea991ae760a4e705635cb5bee59328e02a9
JL-OLEMAR <joseluis19963@gmail.com>
2022-01-19 20:22:23 -0500

    feat(medicos): delete doctors

--- src/app/pages/mantenimientos/medicos/medicos.component.ts
+++ src/app/pages/mantenimientos/medicos/medicos.component.ts
@@ -1,6 +1,8 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
 import { Component, OnDestroy, OnInit } from '@angular/core'
 import { Subscription } from 'rxjs'
 import { delay } from 'rxjs/operators'
+import Swal from 'sweetalert2'
 
 import { Medico } from '../../../interfaces/getAllMedicos.interface'
 import { MedicoService } from '../../../services/medico.service'
@@ -62,5 +64,25 @@ export class MedicosComponent implements OnInit, OnDestroy {
     this._modalImagenService.openModal('medicos', medico._id, medico.img)
   }
 
-  openSweetAlertForm (): void {}
+  deleteMedico (medico: Medico): void {
+    Swal.fire({
+      title: '¿Borrar médico?',
+      text: `Esta a punto de borrar a ${medico.nombre}!`,
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonText: 'Sí, borrarlo!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this._medicoService.deleteDoctor(medico._id)
+          .subscribe(() => {
+            this.getDoctors()
+            Swal.fire(
+              'Médico borrado!',
+              `${medico.nombre} fue eliminado correctamente`,
+              'success'
+            )
+          })
+      }
+    })
+  }
 }
